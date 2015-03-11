2dcc6eabafc3c4ebe123a4afc36e122ce2dae0ef
Henk Erik van der Hoek <mail@henkerikvanderhoek.nl>
2015-03-11 11:58:43 +0100

    Removed implementations

--- src/main/scala/nl/henkerikvanderhoek/example/Sort.scala
+++ src/main/scala/nl/henkerikvanderhoek/example/Sort.scala
@@ -8,7 +8,7 @@ object Sort extends App {
     case Nil     => Nil
     case (x::xs) => {
       val lower  = sort(xs.filter { y => Ordering[A].lt(y,x) })
-      val higher = sort(xs.filter { y => Ordering[A].gteq(y,x) })
+      val higher = sort(xs.filter { y => Ordering[A].gt(y,x) })
 
       lower ++ List(x) ++ higher
     }
@@ -17,32 +17,16 @@ object Sort extends App {
   def propSortAscending[A:Ordering]:List[A] => Boolean =
     xs => sorted(sort(xs))
 
-  def propKeepValues[A:Ordering]:List[A] => Boolean = xs =>
-    sort(xs).foldLeft[Option[List[A]]](Some(xs)) {
-      case (None,      x) => None
-      case (Some(rem), x) => remove(rem)(x)
-    } == Some(List.empty)
-
-
   def propKeepLength[A:Ordering]:List[A] => Boolean =
-    xs => sort(xs).length == xs.length
+    xs => ???
 
 
   Check(propSortAscending: List[Int] => Boolean)
-  Check(propKeepValues:    List[Int] => Boolean)
   Check(propKeepLength:    List[Int] => Boolean)
 
   private def sorted[A:Ordering](xs:List[A]):Boolean = xs match {
     case Nil      => true
     case x::Nil   => true
     case x::y::ys => Ordering[A].lteq(x, y) && sorted(y::ys)
-  }
-
-  private def remove[A](xs:List[A])(y:A):Option[List[A]] = {
-    def go(xs: List[A], ys: List[A]):Option[List[A]] = xs match {
-      case Nil   => None
-      case x::xs => if (x == y) { Some(ys.reverse ++ xs) } else { go(xs, x::ys) }
-    }
-    go(xs, List.empty)
   }
 }
